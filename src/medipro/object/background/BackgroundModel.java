ee4c57cf239c98f009d508b2bf25bb9cafbd0014
eva828Dev <88603178+eva828Dev@users.noreply.github.com>
2023-12-22 14:28:24 +0900

    インポートエラーを修正

--- src/medipro/object/background/BackgroundModel.java
+++ src/medipro/object/background/BackgroundModel.java
@@ -1,12 +1,12 @@
 package medipro.object.background;
 
+import medipro.object.base.World;
 import medipro.object.base.gameobject.GameObjectModel;
-import medipro.world.World;
 
 public class BackgroundModel extends GameObjectModel {
-    //private GameObjectModel model;
-    //private double x = model.x;
-    public BackgroundModel(World world){
+    // private GameObjectModel model;
+    // private double x = model.x;
+    public BackgroundModel(World world) {
         super(world);
         logger.info("BackgroundModel created");
     }
