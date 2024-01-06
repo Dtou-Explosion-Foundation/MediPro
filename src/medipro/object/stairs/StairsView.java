ca980e8eb84e4a5dc228fe4162ee8aba542ed0f9
eva828Dev <88603178+eva828Dev@users.noreply.github.com>
2024-01-06 15:36:58 +0900

    StairsViewをOpenGLに対応

--- src/medipro/object/stairs/StairsView.java
+++ src/medipro/object/stairs/StairsView.java
@@ -5,9 +5,17 @@ import java.awt.geom.AffineTransform;
 import java.awt.image.AffineTransformOp;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
 
 import javax.imageio.ImageIO;
+
+import com.jogamp.opengl.GL4;
+import com.jogamp.opengl.GLAutoDrawable;
+import com.jogamp.opengl.math.Matrix4f;
+import com.jogamp.opengl.util.texture.TextureData;
 
 import medipro.object.base.gameobject.GameObjectModel;
 import medipro.object.base.gameobject.GameObjectView;
@@ -51,6 +59,59 @@ public class StairsView extends GameObjectView {
             g.drawImage(textures[stairsModel.isLeftUp() ? 1 : 0], (int) (-getSpriteWidth() / 2),
                     (int) (-getSpriteHeight() / 2), null);
         }
+    }
+
+    @Override
+    protected String getShaderPath(String ext) {
+        super.getShaderPath(ext);
+        return "shader/gameobject/GameObject" + "." + ext;
+    }
+
+    @Override
+    protected boolean needUpdateTexture() {
+        return false;
+    }
+
+    @Override
+    protected void initTextures(GLAutoDrawable drawable) {
+        GL4 gl = drawable.getGL().getGL4();
+        textureName.clear();
+        gl.glGenTextures(1, textureName);
+        TextureData textureData;
+        try {
+            InputStream textureStream = new FileInputStream("img/stairs.png");
+            BufferedImage bufferedImage = ImageIO.read(textureStream);
+            ByteBuffer buffer = convertBufferedImageToByteBuffer(bufferedImage);
+
+            textureData = new TextureData(gl.getGLProfile(), GL4.GL_RGBA, bufferedImage.getWidth(),
+                    bufferedImage.getHeight(), 0, GL4.GL_RGBA, GL4.GL_UNSIGNED_BYTE, false, false, false, buffer, null);
+        } catch (IOException e) {
+            logger.warning(e.toString());
+            return;
+        }
+
+        gl.glBindTexture(GL4.GL_TEXTURE_2D, textureName.get(0));
+
+        gl.glTexParameteri(GL4.GL_TEXTURE_2D, GL4.GL_TEXTURE_BASE_LEVEL, 0);
+        gl.glTexParameteri(GL4.GL_TEXTURE_2D, GL4.GL_TEXTURE_MAX_LEVEL, 0);
+
+        gl.glTexImage2D(GL4.GL_TEXTURE_2D, 0, textureData.getInternalFormat(), textureData.getWidth(),
+                textureData.getHeight(), 0, textureData.getPixelFormat(), textureData.getPixelType(),
+                textureData.getBuffer());
+    }
+
+    @Override
+    protected Matrix4f getModelMatrix() {
+        StairsModel stairsModel = (StairsModel) model;
+        Matrix4f tempMat = new Matrix4f();
+        Matrix4f modelMat = new Matrix4f() // モデルの座標変換行列
+                .translate((float) model.x, (float) model.y, 0, tempMat) // 座標
+                .scale(getSpriteWidth(), getSpriteHeight(), 1, tempMat)// 基準サイズ
+                .rotate((float) model.rotation, 0, 0, 1, tempMat) // 回転
+                .scale(stairsModel.isLeftUp() ? -1 : 1, 1, 1, tempMat) // 左右反転
+                .scale((float) model.scaleX, (float) model.scaleY, 1, tempMat) // スケーリング
+        ;
+        return modelMat;
     }
 
 }
