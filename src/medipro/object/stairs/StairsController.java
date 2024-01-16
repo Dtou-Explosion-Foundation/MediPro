7c87bb227e57b419b9dcceec1803b836f1fb23c5
eva828Dev <88603178+eva828Dev@users.noreply.github.com>
2024-01-16 12:29:04 +0900

    AutoWalkerで再実装2

--- src/medipro/object/stairs/StairsController.java
+++ src/medipro/object/stairs/StairsController.java
@@ -1,7 +1,5 @@
 package medipro.object.stairs;
 
-import java.awt.geom.Point2D;
-import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
 import javax.swing.JPanel;
@@ -12,7 +10,9 @@ import medipro.object.base.gameobject.GameObjectController;
 import medipro.object.base.gameobject.GameObjectModel;
 import medipro.object.manager.gamemanager.GameManagerController;
 import medipro.object.overlay.blackfilter.BlackFilterController;
+import medipro.object.player.AutoWalker;
 import medipro.object.player.PlayerController;
+import medipro.object.player.PlayerModel;
 
 public class StairsController extends GameObjectController {
 
@@ -20,8 +20,15 @@ public class StairsController extends GameObjectController {
     private BlackFilterController blackFilterController;
     private GameManagerController gameManagerController;
 
+    private static AutoWalker startAutoWalker;
+
     public StairsController(GameObjectModel model) {
         super(model);
+        StairsModel stairsModel = (StairsModel) model;
+        if (model.x > 0)
+            logger.info("Trigger Range: " + (model.x - stairsModel.getTriggerRange() / 2) + " ~ "
+                    + (model.x + stairsModel.getTriggerRange() / 2));
+
     }
 
     @Override
@@ -42,9 +49,20 @@ public class StairsController extends GameObjectController {
         if (gameManagerControllers.size() > 0) {
             this.gameManagerController = gameManagerControllers.get(0);
         }
+
+        if (startAutoWalker != null && playerController != null) {
+            playerController.pushAutoWalker(startAutoWalker);
+            startAutoWalker = null;
+            isPlayerOnStairs = true;
+
+            if (blackFilterController != null && startBlackFilterDuration > 0)
+                blackFilterController.blackOut(startBlackFilterDuration);
+        }
+
     }
 
-    private boolean isPlayerGoingUp = false;
+    private boolean isPlayerOnStairs = true;
+    private static float startBlackFilterDuration = 0;
 
     @Override
     public void update(double dt) {
@@ -52,33 +70,48 @@ public class StairsController extends GameObjectController {
             return;
 
         StairsModel stairsModel = (StairsModel) model;
-        float handleRange = stairsModel.getWidth() / 2;
-        float playerX = (float) playerController.model.x;
-        if (!isPlayerGoingUp && playerX > model.x - handleRange && playerX < model.x + handleRange) {
-            isPlayerGoingUp = true;
+        PlayerModel playerModel = (PlayerModel) playerController.model;
+        float triggerRange = stairsModel.getTriggerRange() / 2;
+        if (playerModel.x > model.x - triggerRange && playerModel.x < model.x + triggerRange) {
+            // if (isPlayerOnStairs || playerModel.isPlayerAutoWalking())
+            if (isPlayerOnStairs)
+                return;
+
+            isPlayerOnStairs = true;
+
+            AutoWalker endAutoWalker = new AutoWalker(playerModel.x, playerModel.y,
+                    model.x + stairsModel.getWidth() / 2 * (stairsModel.isLeftUp() ? -1 : 1),
+                    model.y + stairsModel.getHeight() / 2);
+            double endDuration = endAutoWalker.setSpeed(playerModel.speedLimitX);
             if (blackFilterController != null)
-                blackFilterController.blackIn(4f);
-            playerController.pushAutoWalker( // 自動で階段を上る
-                    new Point2D.Double(model.x + handleRange * (stairsModel.isLeftUp() ? -1 : 1),
-                            model.y + stairsModel.getHeight() / 2),
-                    4, t -> t, () -> {
-                        // if (blackFilterController != null)
-                        //     blackFilterController.blackOut(2f);
-                        // if (gameManagerController != null)
-                        //     gameManagerController.nextFloor();
-                        World newWorld;
-                        try {
-                            newWorld = this.model.world.getClass().getDeclaredConstructor(JPanel.class)
-                                    .newInstance(this.model.world.getPanel());
-                        } catch (InstantiationException | IllegalAccessException | IllegalArgumentException
-                                | InvocationTargetException | NoSuchMethodException | SecurityException e) {
-                            logger.severe(e.getMessage());
-                            e.printStackTrace();
-                            return;
-                        }
-                        IGamePanel panel = (IGamePanel) this.model.world.getPanel();
-                        panel.setWorld(newWorld);
-                    });
+                blackFilterController.blackIn((float) endDuration);
+            final double playerY = playerModel.y;
+            endAutoWalker.setCallback(() -> {
+                startAutoWalker = new AutoWalker(
+                        model.x + stairsModel.getWidth() / 2 * (stairsModel.isLeftUp() ? -1 : 1),
+                        model.y - stairsModel.getHeight() / 2,
+                        model.x + stairsModel.getTriggerRange() / 2 * 1.2 * (stairsModel.isLeftUp() ? 1 : -1), playerY);
+
+                startBlackFilterDuration = (float) startAutoWalker.setSpeed(playerModel.speedLimitX);
+
+                if (gameManagerController != null)
+                    gameManagerController.nextFloor();
+
+                World newWorld;
+                try {
+                    newWorld = this.model.world.getClass().getDeclaredConstructor(JPanel.class)
+                            .newInstance(this.model.world.getPanel());
+                } catch (Exception e) {
+                    logger.severe(e.getMessage());
+                    e.printStackTrace();
+                    return;
+                }
+                IGamePanel panel = (IGamePanel) this.model.world.getPanel();
+                panel.setWorld(newWorld);
+            });
+            playerController.pushAutoWalker(endAutoWalker);
+        } else {
+            isPlayerOnStairs = false;
         }
     }
 }
