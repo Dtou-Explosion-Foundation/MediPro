d3139fd37a6e0af7ce05d54cf617264b47502a41
eva828Dev <88603178+eva828Dev@users.noreply.github.com>
2024-01-22 12:41:35 +0900

    異変生成処理をTextureObjectControllerからTextureChangeAnomalyに移行

--- src/medipro/object/ornament/texture/TextureObjectController.java
+++ src/medipro/object/ornament/texture/TextureObjectController.java
@@ -1,13 +1,13 @@
 package medipro.object.ornament.texture;
 
-import medipro.object.AnomalyListener;
 import medipro.object.base.gameobject.GameObjectController;
 import medipro.object.base.gameobject.GameObjectModel;
 
 /**
  * テクスチャのコントローラ.
  */
-public class TextureObjectController extends GameObjectController implements AnomalyListener {
+public class TextureObjectController extends GameObjectController {
+
     /**
      * テクスチャのコントローラを生成する.
      * 
@@ -22,41 +22,4 @@ public class TextureObjectController extends GameObjectController implements Ano
 
     }
 
-    /**
-     * 異変が発生しているかどうか.
-     */
-    private static boolean anomalyOccurred = false;
-
-    @Override
-    public boolean canAnomalyOccurred() {
-        // return !anomalyOccurred;
-        // TODO: 異変の量が少ないので再度発生を一時的に許可している
-        return true;
-    }
-
-    @Override
-    public void onAnomalyOccurred(int level) {
-        anomalyOccurred = true;
-        ((TextureObjectModel) model).setTextureIndex(level);
-    }
-
-    @Override
-    public void onAnomalyFinished() {
-        ((TextureObjectModel) model).setTextureIndex(0);
-    }
-
-    @Override
-    public int minAnomalyLevel() {
-        return 1;
-    }
-
-    @Override
-    public int maxAnomalyLevel() {
-        return ((TextureObjectModel) model).getTexturePaths().length - 1;
-    }
-
-    @Override
-    public int getOccurredChance() {
-        return ((TextureObjectModel) model).getOccurredChance();
-    }
 }
